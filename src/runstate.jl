41bc18f0cc8bfb626547c67d1fea144150dd1b42
jherzog99 <j.herzog@tu-berlin.de>
2024-06-13 17:57:55 +0200

    added explanatory comments to the RunState struct

--- src/runstate.jl
+++ src/runstate.jl
@@ -1,14 +1,15 @@
 struct RunState
-    x::Array{float}
-    xb::Array{float}
-    xb2::Array{float}
-    hb::Array{float}
-    hb2::Array{float}
-    q::Array{float}
-    k::Array{float}
-    v::Array{float}
-    att::Array{float,2}
-    logits::Array{float}
-    key_cache::Array{float,3}
-    value_cache::Array{float,3}
+    x::Array{float} # activation at current time stamp (dim,)
+    xb::Array{float} # same, but inside a residual branch (dim,)
+    xb2::Array{float} # an additional buffer just for convenience (dim,)
+    hb::Array{float} # buffer for hidden dimension in the ffn (hidden_dim,)
+    hb2::Array{float} # buffer for hidden dimension in the ffn (hidden_dim,)
+    q::Array{float} # query (dim,)
+    k::Array{float} # key (dim,)
+    v::Array{float} # value (dim,)
+    att::Array{float,2} # buffer for scores/attention values (n_heads, seq_len)
+    logits::Array{float} # output logits
+    # kv cache
+    key_cache::Array{float,3} # (layer, seq_len, dim)
+    value_cache::Array{float,3} # (layer, seq_len, dim)
 end
