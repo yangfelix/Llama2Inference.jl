4892bfaf528a69ce8ed01789e23f6bdc0a9ad6c9
jherzog99 <j.herzog@tu-berlin.de>
2024-06-15 23:33:17 +0200

    made RunState mutable to overwrite fields & changed the dimensionality of key/value cache to match the calloc call in line 86/87 in run.c

--- src/runstate.jl
+++ src/runstate.jl
@@ -1,4 +1,5 @@
-struct RunState
+# 
+mutable struct RunState
     x::Array{float} # activation at current time stamp (dim,)
     xb::Array{float} # same, but inside a residual branch (dim,)
     xb2::Array{float} # an additional buffer just for convenience (dim,)
@@ -10,6 +11,10 @@ struct RunState
     att::Array{float,2} # buffer for scores/attention values (n_heads, seq_len)
     logits::Array{float} # output logits
     # kv cache
-    key_cache::Array{float,3} # (layer, seq_len, dim)
-    value_cache::Array{float,3} # (layer, seq_len, dim)
+    # corresponding to line 86 in run.c the actual dimensionality of key-cache is (layer, seq, kv_dim)
+    # s->key_cache = calloc(p->n_layers * p->seq_len * kv_dim, sizeof(float));
+    # in the calloc function kv_dim is used instead of dim like in the dimensionality describtion
+    key_cache::Array{float,3} # (layer, seq_len, kv_dim)
+    # same for value_cache in line 87
+    value_cache::Array{float,3} # (layer, seq_len, kv_dim)
 end
