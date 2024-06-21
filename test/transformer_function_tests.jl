7a8a6a325ba6124938163d4a7c3d4d265e028057
jherzog99 <j.herzog@tu-berlin.de>
2024-06-21 16:44:49 +0200

    changed the tolerance to 1e-5 because 1e-5 is used in the implementation of rmsnorm in run.c

--- test/transformer_function_tests.jl
+++ test/transformer_function_tests.jl
@@ -17,14 +17,14 @@ using Llama2Inference
                 weight = [0.2, 0.4, 0.1, 0.2, 0.1]
                 expected = [0.0157111,  0.0942664,  0.1571106, -0.0078555, -0.1571106] 
                 rmsnorm!(x, x, weight)
-                @test isapprox(x, expected, atol=1e-6)
+                @test isapprox(x, expected, atol=1e-5)
                 
                 # Test when rmsnorm is computed over a subset of x, rest of x should remain unchanged
                 x = [0.1, 0.3, 2.0, -0.05, -2.0]
                 weight = [0.2, 0.4, 0.5]
                 expected = [0.0171080, 0.1026479, 0.8553989] 
                 rmsnorm!(@view(x[begin:3]), @view(x[begin:3]), weight)
-                @test (isapprox( @view(x[begin:3]), expected, atol=1e-6) && isapprox(@view(x[4:end]), [-0.05, -2.0]))
+                @test (isapprox( @view(x[begin:3]), expected, atol=1e-5) && isapprox(@view(x[4:end]), [-0.05, -2.0]))
             end
         end
         @testset "softmax! Tests" begin
