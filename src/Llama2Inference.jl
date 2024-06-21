8ae3a16efe587cd3dc8d5cfd5ccc56ae3a878613
jherzog99 <j.herzog@tu-berlin.de>
2024-06-21 16:11:24 +0200

    Changed the Type in runstate to an actual type and added 0-initialization for Arrays in RunState from config

--- src/Llama2Inference.jl
+++ src/Llama2Inference.jl
@@ -19,5 +19,6 @@ export read_checkpoint, Transformer, forward, rmsnorm!, softmax!, generate
 export Config, set_config_vocab_size, read_config
 export TransformerWeights, get_weights, memory_map_weights
 export replace_top_pair!, Tokenizer, get_most_common_pair, count_consecutive, decoding, encoding
+export RunState
 
 end
