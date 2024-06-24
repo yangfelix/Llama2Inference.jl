bc19c696280a87cc1a0960838f998430a680b329
jherzog99 <j.herzog@tu-berlin.de>
2024-06-25 00:06:43 +0200

    adapted the forward test to the new signature and added a test for the generate function

--- test/transformer_function_tests.jl
+++ test/transformer_function_tests.jl
@@ -2,8 +2,8 @@ using Test
 using Llama2Inference
 using DelimitedFiles
 
-@testset "Transformer Tests" begin
-    @testset "Single Function Tests" begin
+@testset "transformer.jl Tests" begin
+    @testset "Math Function Tests" begin
         @testset "rmsnorm! Tests" begin
             # Test rmsnorm!(out, x, weight)
             @testset "Throw Error Tests" begin
@@ -51,22 +51,51 @@ using DelimitedFiles
             end
         end
     end
-    @testset "forward test" begin
-        @testset "forward with token=1 (empty string token) and pos=1" begin
-            # This test uses the run.c file from https://github.com/karpathy/llama2.c/blob/master/run.c
-            # The command "./run stories15M.bin" was used to execute the run.c file
-            # The execution was stopped after the first logits are returned from the forward function
-            # The corresponding logits are of dimension (vocab_size,) = (32000,) and are stored in empty_prompt_logits_first_iteration.csv
-            # The output of the forward function of the transformer.jl are compared with the values stored in empty_prompt_logits_first_iteration.csv
-            # To match the setup of the run.c file, the token is set to 1 and the pos is set to 1 (in the run.c file the token is 1 and the pos is 0)
-            # The difference in the pos is due to the 1-indexed based system in Julia
-            config, weights = read_checkpoint("../stories15M.bin")
-            state = RunState(config)
-            token = 1
-            pos = 1
-            logits = forward(config, weights, state, token, pos)
-            expected_logits = readdlm("../empty_prompt_logits_first_iteration.csv", Float32)
-            @test maximum(abs.(logits - expected_logits)) < 1e-4
+    @testset "Transformer Tests" begin
+        # transformer and tokenizer are the same for the forward & generate tests
+        config, weights = read_checkpoint("../stories15M.bin")
+        transformer = Transformer(config, weights)
+        tokenizer = build_tokenizer("../tokenizer.bin", Int(config.vocab_size))
+
+        @testset "forward test" begin
+            @testset "forward with token=2 (empty string token) and pos=1" begin
+                # This test uses the run.c file from https://github.com/karpathy/llama2.c/blob/master/run.c
+                # The command "./run stories15M.bin" was used to execute the run.c file
+                # The execution was stopped after the first logits are returned from the forward function
+                # The corresponding logits are of dimension (vocab_size,) = (32000,) and are stored in empty_prompt_logits_first_iteration.csv
+                # The output of the forward function of the transformer.jl are compared with the values stored in empty_prompt_logits_first_iteration.csv
+                # To match the setup of the run.c file, the token is set to 1 and the pos is set to 1 (in the run.c file the token is 1 and the pos is 0)
+                # The difference in the pos is due to the 1-indexed based system in Julia
+                sampler = Sampler(Int(config.vocab_size), 1.f0, 0.9f0)
+                state = RunState(config)
+                token = 2
+                pos = 1
+                logits = forward(transformer, state, token, pos)
+                expected_logits = readdlm("../empty_prompt_logits_first_iteration.csv", Float32)
+                @test maximum(abs.(logits - expected_logits)) < 1e-4
+            end
+        end
+
+        @testset "generate test" begin
+            @testset "generate with token=1 (empty string token) and pos=1" begin
+                # This test uses the run.c file from https://github.com/karpathy/llama2.c/blob/master/run.c
+                # The command
+                # ./run stories15M.bin -n 22 -t 0.0 -i "The universe"
+                # was used for execution of the run.c file, -t 0.0 enables argmax sampling which is deterministic and therefore can be used for comparison
+                # capture the output of the generate function and compare it with the expected output coming from the run.c file execution
+                sampler = Sampler(Int(config.vocab_size), 0.0f0, 0.9f0)
+                expected_output = "The universe was bright and full of stars. Every night, the stars would twinkle and shine.\n"
+                
+                # capturing output of genereate funtion
+                original_stdout = stdout
+                (rd, wr) = redirect_stdout()
+                generate(transformer, tokenizer, sampler, 23; prompt="The universe")
+                redirect_stdout(original_stdout)
+                close(wr)
+                output = read(rd, String)
+                
+                @test output == expected_output
+            end
         end
     end
 end
