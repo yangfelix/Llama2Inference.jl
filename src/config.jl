d4c90c7172357c7b9f13dffb87648b4aef2d94c9
Yang Felix Wang <yangfelix@posteo.net>
2024-06-30 16:19:50 +0200

    added docs for config

--- src/config.jl
+++ src/config.jl
@@ -1,3 +1,24 @@
+"""
+    Config{T<:Integer}
+
+Struct to hold the configuration of the [`Transformer`](@ref) used for inference. It contains the following fields:
+
+- dim::T: transformer dimension
+- hidden_dim::T: for ffn layers
+- n_layers::T: number of layers
+- n_heads::T: number of query heads
+- n_kv_heads::T: number of key/value heads
+- vocab_size::T: vocabulary size
+- seq_len::T: max sequence length
+
+It is possible to construct a `Config` using the constructor 
+
+    Config{T}(dim::T, hidden_dim::T, n_layers::T, n_heads::T, n_kv_heads::T, vocab_size::T, seq_len::T)
+
+but it is recommended to use the data format defined by [Andrew Karpathy](https://github.com/karpathy/llama2.c)
+to store both the configuration and weights of the transformer used and read the config and weights using 
+[`read_checkpoint`](@ref).
+"""
 struct Config{T<:Integer}
     dim::T          # transformer dimension
     hidden_dim::T   # for ffn layers
@@ -8,7 +29,12 @@ struct Config{T<:Integer}
     seq_len::T      # max sequence length
 end
 
-function read_config(T::Type{<:Integer}, file::IO)
+"""
+    read_config(T::Type{<:Integer}, file::IO)::Config{T}
+
+Reads from `file` to create a [`Config{T}`](@ref).
+"""
+function read_config(T::Type{<:Integer}, file::IO)::Config{T}
     num_bytes = sizeof(Config{T})
     buffer = Vector{UInt8}(undef, num_bytes)
     read!(file, buffer)
@@ -16,6 +42,11 @@ function read_config(T::Type{<:Integer}, file::IO)
     return config
 end
 
+"""
+    set_config_vocab_size(config::Config{T}, vocab_size::T)
+
+Creates a new [`Config{T}`](@ref) with the field `vocab_size` updated to `vocab_size`.
+"""
 function set_config_vocab_size(config::Config{T}, vocab_size::T) where {T<:Integer}
     return Config(
         config.dim,
